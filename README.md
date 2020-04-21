cd855a674712fa7bf191862db4b536c398091ee1
CrossAxis98 <roberto2611@gmail.com>
2020-04-21 11:55:25 +0200

    Zmieniony projekt, dodane polaczanie z baza

--- README.md
+++
@@ -1,6 +0,0 @@
-# dotNetJava
-Maciej Popieluch, Robert Krzyżoś
-
-Projekt dotNet + Oracle 
-
-![](Nauka/Nauka/images/aktualnyWyglad.png)
---
+++ dotNetProjekt/CzasPracy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dotNetProjekt
+{
+    public class CzasPracy
+    {
+        public int CzasPracyId { get; set; }
+        public Pracownicy Pracownicy { get; set; }
+
+        public int PracownicyId { get; set; }
+
+        public DateTime CzasRozpoczęcia { get; set; }
+
+        public DateTime CzasZakończenia { get; set; }
+    }
+}
